cee7f5f6b7fa48d8f5f624502a27f15ee308ad28
静听风吟 <aininot260@126.com>
2025-11-22 12:10:21 +0800

    推送！

--- download_model.py
+++ download_model.py
@@ -6,7 +6,7 @@ os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # 模型名称
-model_name = "mistralai/Mistral-7B-v0.1"
+model_name = "facebook/opt-1.3b"
 # 本地保存路径（可自定义，例如 "./local-distilgpt2"）
 local_model_path = "./local-model"
 
